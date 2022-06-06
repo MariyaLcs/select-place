01999560afe574661acc80f5df7896300949f859
Mariya Podosinova <mariya.podosinova@gmail.com>
2022-06-06 20:44:45 +0100

    add Axios to fetch coordinates for an entered address

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const YOUR_API_KEY = "AIzaSyDTbpL4";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  //todo send this to Google api
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${YOUR_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Something went wrong");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener("submit");
+form.addEventListener("submit", searchAddressHandler);
